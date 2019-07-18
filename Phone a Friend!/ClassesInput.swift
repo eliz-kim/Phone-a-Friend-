30b8713e847da4ccdce3b7b4d4c9c64b8952a83a
Girls Who Code <GWC@cs81-ms-crt-17.local>
2019-07-18 15:29:11 -0400

    This is the final app.

---
+++ Phone a Friend!/ClassesInput.swift
@@ -0,0 +1,58 @@
+//
+//  ClassesInput.swift
+//  Phone a Friend!
+//
+//  Created by Girls Who Code on 7/18/19.
+//  Copyright © 2019 Girls Who Code. All rights reserved.
+//
+
+import UIKit
+
+class ClassesInput: UIViewController {
+    
+    var subjectNumber: Int = 0
+    
+    struct Class {
+        let Title: String?
+        let Announcements: String?
+    }
+    
+    var currentClass: Class?
+    
+    var classesArray: [Class] = [
+        Class(Title: "Math",
+              Announcements: "Etc"),
+        Class(Title: "English",
+              Announcements: "Etc"),
+        Class(Title: "History",
+              Announcements: "Etc"),
+        Class(Title: "Science",
+              Announcements: "Etc"),
+        Class(Title: "Language",
+              Announcements: "Etc"),
+        ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    
+    @IBAction func subjectPressed(_ sender: Any) {
+        var button = sender as! UIButton
+        
+        subjectNumber = button.tag
+        performSegue(withIdentifier: "sgShowSubject", sender: nil)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! SubjectInput
+        vc.subjectNumber = subjectNumber
+        
+    }
+    
+}
---
+++ Phone a Friend!/SubjectInput.swift
@@ -0,0 +1,42 @@
+//
+//  SubjectInput.swift
+//  Phone a Friend!
+//
+//  Created by Girls Who Code on 7/18/19.
+//  Copyright © 2019 Girls Who Code. All rights reserved.
+//
+
+import UIKit
+
+class SubjectInput: UIViewController {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var subjectImage:
+    
+    UIImageView!
+    var subjects: [String] = ["MATH", "ENGLISH", "HISTORY", "SCIENCE", "LANGUAGE"]
+    var images: [String] = ["MATH", "ENGLISH", "HISTORY", "SCIENCE", "LANGUAGE"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = subjects[subjectNumber]
+        subjectImage.image = UIImage(named: images[subjectNumber])
+        // Do any additional setup after loading the view.
+    }
+    
+    var subjectNumber: Int = 0
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
